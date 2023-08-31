b7516befd49c3de4b5232288987ad0b47bab3c90
jesusignazio <jesusignazio@gmail.com>
2023-08-31 12:15:20 +0200

    2.0

---
+++ wololo-bot-commands.py
@@ -0,0 +1,50 @@
+import typing
+import os
+
+from discord import app_commands
+from discord.ext import commands
+import discord
+
+description = "DK_Wololo"
+TOKEN = 'MTE0NjExMDE2MDcyNTYyMjg0NQ.Gz4VjC.-VMX53nHhI6deUQl5QjpT5vtQ-dn4bDn-bVRG4'
+
+def log(message):
+    print(message)
+
+class WololoCOG(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def add_ranking(self, ctx, arg1=None, arg2=None):
+        log("Comando add_ranking")
+        if isinstance(ctx.message.channel,
+                      discord.channel.DMChannel) or ctx.message.author.id == 401762141906141184 or ctx.message.author.id == 292314272640401409 or ctx.message.author.id == 407631373369737217 or ctx.message.author.id == 618398991700459520:
+            print("Granted!")
+
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name="add_ranking", description="Añadir miembro al ranking")
+@app_commands.describe(profile_id="Profile id de aoe2.net", name="Nombre", discord_id="ID de discord")
+async def add_ranking(interaction: discord.Interaction, profile_id: typing.Optional[str], name: typing.Optional[str], discord_id: typing.Optional[str]):
+    log("Comando add_ranking")
+    if interaction.user.id == 474968187637596160 or interaction.user.id == 401762141906141184 or interaction.user.id == 292314272640401409 or interaction.user.id == 618398991700459520:
+        await interaction.response.defer()
+        if profile_id != None and name != None and discord_id != None:
+            with open(os.path.realpath(os.path.dirname(__file__)) + "/watched.txt", 'a') as f:
+                f.write(profile_id + "&&&" + discord_id + "&&&" + name + "&&&" + str(
+                    0) + "&&&" + str(0) + "\n")
+                await interaction.followup.send("Añadido " + name + " al ranking.")
+                print("Añadiddo " + name + " al ranking.")
+
+bot.run(TOKEN)
---
+++ wololo-bot-ranking-message.py
@@ -0,0 +1,155 @@
+import time
+import discord
+import os
+import requests
+
+TOKEN = 'MTE0NjExMDE2MDcyNTYyMjg0NQ.Gz4VjC.-VMX53nHhI6deUQl5QjpT5vtQ-dn4bDn-bVRG4'
+CHANNEL_ID = 799746255873376297
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+list_players = []
+
+class PlayerWatched:
+    def __init__(self, profile_id, discord_id, discord_name, last_rm_elo, new_rm_elo, last_tg_elo, new_tg_elo):
+        self.profile_id = profile_id
+        self.discord_id = discord_id
+        self.discord_name = discord_name
+
+        self.last_rm_elo = last_rm_elo
+        self.new_rm_elo = new_rm_elo
+        self.max_rm_elo = ""
+        self.last_tg_elo = last_tg_elo
+        self.new_tg_elo = new_tg_elo
+        self.max_tg_elo = ""
+
+        self.url_rm = "https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=3&profile_id=" + str(profile_id) + "&count=1"
+        self.url_tg = "https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=4&profile_id=" + str(profile_id) + "&count=1"
+
+    def get_rm_elo_diff(self):
+        if self.last_rm_elo == 0:
+            return ""
+        else:
+            diff = self.new_rm_elo - self.last_rm_elo
+            if diff < 0:
+                return " (" + str(diff) + ") "
+            elif diff > 0:
+                return " (+" + str(diff) + ") "
+            else:
+                return ""
+    def get_tg_elo_diff(self):
+        if self.last_tg_elo == 0:
+            return ""
+        else:
+            diff = self.new_tg_elo - self.last_tg_elo
+            if diff < 0:
+                return " (" + str(diff) + ") "
+            elif diff > 0:
+                return " (+" + str(diff) + ") "
+            else:
+                return ""
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print("Running")
+        with open(os.path.realpath(os.path.dirname(__file__)) + "/watched.txt") as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.replace("\n", "")
+                words = line.split("&&&")
+                profile_id = words[0]
+                discord_id = words[1]
+                discord_name = words[2]
+                last_elo = int(words[3])
+                last_tg_elo = int(words[4])
+                new_rm_elo = None
+                new_tg_elo = None
+                player_watched = PlayerWatched(profile_id, discord_id, discord_name, last_elo, new_rm_elo, last_tg_elo, new_tg_elo)
+                list_players.append(player_watched)
+
+        for p in list_players:
+            try:
+                print("Getting " + p.discord_name)
+                """RM elo"""
+                resp = requests.get(url=p.url_rm)
+                data = resp.json()
+                new_rm_elo = int(data[0]['rating'])
+                p.new_rm_elo = int(new_rm_elo)
+                """TG elo"""
+                resp = requests.get(url=p.url_tg)
+                data = resp.json()
+                new_tg_elo = int(data[0]['rating'])
+                p.new_tg_elo = int(new_tg_elo)
+                #TODO get max ELO
+
+            except Exception as e:
+                continue
+
+            time.sleep(1)
+
+        """Sort players by new RM elo rating"""
+        list_players.sort(key=lambda x: x.new_rm_elo, reverse=True)
+
+        i = 1
+        message_rm = "```"
+        for p in list_players:
+            message_rm = message_rm + "{:02d}".format(i) + " " + str(p.new_rm_elo).zfill(4) + " " + p.discord_name + p.get_rm_elo_diff()
+            if i == 1:
+                message_rm = message_rm + "🥇"
+            elif i == 2:
+                message_rm = message_rm + "🥈"
+            elif i == 3:
+                message_rm = message_rm + "🥉"
+            i += 1
+            message_rm = message_rm + "\n"
+        message_rm = message_rm + "```"
+
+        """Sort players by new TG elo rating"""
+        list_players.sort(key=lambda x: x.new_tg_elo, reverse=True)
+
+        i = 1
+        message_tg = "```"
+        for p in list_players:
+            message_tg = message_tg + "{:02d}".format(i) + " " + str(p.new_tg_elo).zfill(4) + " " + p.discord_name + p.get_tg_elo_diff()
+            if i == 1:
+                message_tg = message_tg + "🥇"
+            elif i == 2:
+                message_tg = message_tg + "🥈"
+            elif i == 3:
+                message_tg = message_tg + "🥉"
+            i += 1
+            message_tg = message_tg + "\n"
+        message_tg = message_tg + "```"
+
+        channel_to = await bot.fetch_channel(CHANNEL_ID)
+
+        print("Purge channel content")
+        try:
+            await channel_to.purge(limit=2)
+        except Exception as e:
+            print(e)
+
+        print("Sending message RM")
+        embed_rm = discord.Embed(title="Ranking RM 1vs1", description=message_rm, color=0x992d22)
+        embed_rm.set_thumbnail(url="https://cdn.discordapp.com/attachments/1049283724497404004/1049284070665900043/logotipo-dark-knight-knight-esport_100659-74_1.png")
+        await channel_to.send(embed=embed_rm)
+
+        print("Sending message TG")
+        embed_tg = discord.Embed(title="Ranking TG", description=message_tg, color=0x1f8b4c)
+        embed_tg.set_thumbnail(url="https://cdn.discordapp.com/attachments/1049283724497404004/1049284070665900043/logotipo-dark-knight-knight-esport_100659-74_1.png")
+        await channel_to.send(embed=embed_tg)
+
+        """Clear watched.txt file"""
+        with open(os.path.realpath(os.path.dirname(__file__)) + "/watched.txt", 'w') as f:
+            pass
+
+        """Save new content to watched.txt"""
+        for p in list_players:
+            with open(os.path.realpath(os.path.dirname(__file__)) + "/watched.txt", 'a') as f:
+                f.write(p.profile_id + "&&&" + p.discord_id + "&&&" + p.discord_name + "&&&" + str(p.new_rm_elo) + "&&&" + str(p.new_tg_elo) + "\n")
+        exit()
+
+bot = MyClient(intents=intents)
+bot.run(TOKEN)
